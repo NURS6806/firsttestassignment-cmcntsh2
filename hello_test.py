1ef8e5baa291e061829afe92da366f315298e92b
cmcntsh <32034299+cmcntsh@users.noreply.github.com>
2021-05-02 08:52:15 -0600

    Update hello_test.py

--- hello_test.py
+++ hello_test.py
@@ -2,3 +2,9 @@ import hello;
 
 def test_hello():
     assert hello.hello_world() == "Hello World!"
+    
+def test_add():
+    assert hello.add(1,2) == 3
+    
+def test_diff():
+    assert hello.diff(1,2) == -1
